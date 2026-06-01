e8392ac7489150cc48ace174b926e2ff85ccb6c1
lotion-cmd <adrian.j.slywotzky@gmail.com>
2026-06-01 23:51:20 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,17 @@
+const CACHE_VERSION = 'v2';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('push', event => {
   let title = '🎁 新しいギフトコード！';
   let body = '新しいコードが追加されました。サイトを確認してください。';
